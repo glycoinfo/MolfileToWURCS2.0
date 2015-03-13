46c3cc3d4aa29b18038939ac5c416ed846da23dd
matsubara <matsubara@150.29.191.220>
2015-03-13 10:44:12 +0900

    Move WURCSGraph system to WURCSFramework

--- MolfileToWURCS2/src/org/glycoinfo/WURCSFramework/wurcsgraph/visitor/WURCSVisitable.java
+++
@@ -1,7 +0,0 @@
-package org.glycoinfo.WURCSFramework.wurcsgraph.visitor;
-
-
-public interface WURCSVisitable {
-    public void accept (WURCSVisitor a_objVisitor) throws WURCSVisitorException;
-
-}
